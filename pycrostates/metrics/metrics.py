5809300f1f6cf619f6ec10131ff59692ce97a00b
vferat <victor.ferat@live.fr>
2021-01-19 12:30:40 +0100

    RESET everything

--- pycrostates/metrics/metrics.py
+++ pycrostates/metrics/metrics.py
@@ -1,14 +1,12 @@
 import itertools
-import numpy as np
-import mne
 
-def compute_metrics(
-    labels: np.ndarray,
-    states: np.ndarray,
-    raw: mne.io.RawArray,
-    norm_gfp: bool = True,
-    state_as_index: bool = False,
-) -> dict:
+import mne
+import numpy as np
+
+
+def compute_metrics(labels:np.ndarray, states:np.ndarray,
+                    raw:mne.io.RawArray, norm_gfp:bool = True,
+                    state_as_index:bool=False) -> dict:
     """[summary]
 
     Args:
@@ -25,68 +23,60 @@ def compute_metrics(
         gfp = gfp / np.linalg.norm(gfp)
 
     n_states = states.shape[0]
-<<<<<<< HEAD
-    states_names = [f"state_{s+1}" for s in range(n_states)]
-
-    segments = [(s, list(group)) for s, group in itertools.groupby(labels)]
-    good_segments = [
-        (s, list(group)) for s, group in itertools.groupby(labels) if s != 0
-    ]
-
-=======
     states_names = [f'state_{s+1}' for s in range(n_states)]
-    
+
     segments = [(s, list(group)) for s,group in itertools.groupby(labels)]
     good_segments = [(s, list(group)) for s,group in itertools.groupby(labels) if s != 0]
     
->>>>>>> parent of 146fe28... Add support for Epochs and Evoked
     d_ = {}
     ds = list()
     for s, state in enumerate(states):
         d = {}
         state_name = states_names[s]
-        d["state"] = state_name
-
-        labeled_tp = data.T[np.argwhere(labels == s + 1)][:, 0, :].T
-        labeled_gfp = gfp[np.argwhere(labels == s + 1)][:, 0]
+        d['state'] = state_name
+        
+        labeled_tp = data.T[np.argwhere(labels == s+1)][:,0,:].T
+        labeled_gfp = gfp[np.argwhere(labels == s+1)][:,0]
         stack = np.vstack([state, labeled_tp.T])
         corr = np.corrcoef(stack)[0][1:]
         abs_corr = np.abs(corr)
-        gev = abs_corr * labeled_gfp ** 2 / np.sum(gfp ** 2)
-        d["dist_gev"] = gev
-        d["dist_corr"] = abs_corr
-        d["gev"] = np.sum(gev)
-        d["mean_corr"] = np.mean(abs_corr)
-        d["ev"] = np.sum(abs_corr) / len(gfp)
-        s_segments = np.array([len(group) for s_, group in segments if s_ == s + 1])
-        occurence = len(s_segments) / len(good_segments)
+        gev = abs_corr *  labeled_gfp**2 / np.sum(gfp ** 2)
+        d['dist_gev'] =  gev
+        d['dist_corr'] = abs_corr
+        d['gev'] = np.sum(gev)
+        d['mean_corr'] = np.mean(abs_corr)
+        d['ev'] = np.sum(abs_corr) / len(gfp)
+        s_segments = np.array([len(group) for s_, group in segments if s_ == s+1])
+        occurence = len(s_segments) /len(good_segments)
         timecov = np.sum(s_segments) / len(np.where(labels != 0)[0])
-        durs = s_segments / raw.info["sfreq"]
-        d["timecov"] = timecov
-        d["meandurs"] = np.mean(durs)
-        d["dist_durs"] = durs
-        d["occurences"] = occurence
+        durs = s_segments / raw.info['sfreq']
+        d['timecov'] =  timecov
+        d['meandurs'] = np.mean(durs)
+        d['dist_durs'] = durs
+        d['occurences'] = occurence
         ds.append(d)
+        
+        d_[f'{state_name}_dist_gev'] = gev
+        d_[f'{state_name}_dist_corr'] = abs_corr
+        d_[f'{state_name}_gev'] = np.sum(gev)
+        d_[f'{state_name}_mean_corr'] = np.mean(abs_corr)
+        d_[f'{state_name}_ev'] =np.sum(abs_corr) / len(gfp)
+        d_[f'{state_name}_timecov'] = timecov
+        d_[f'{state_name}_meandurs'] = np.mean(durs)
+        d_[f'{state_name}_dist_durs'] = durs
+        d_[f'{state_name}_occurences'] = occurence
 
-        d_[f"{state_name}_dist_gev"] = gev
-        d_[f"{state_name}_dist_corr"] = abs_corr
-        d_[f"{state_name}_gev"] = np.sum(gev)
-        d_[f"{state_name}_mean_corr"] = np.mean(abs_corr)
-        d_[f"{state_name}_ev"] = np.sum(abs_corr) / len(gfp)
-        d_[f"{state_name}_timecov"] = timecov
-        d_[f"{state_name}_meandurs"] = np.mean(durs)
-        d_[f"{state_name}_dist_durs"] = durs
-        d_[f"{state_name}_occurences"] = occurence
-
+        
     d = {}
     s_segments = [len(group) for s_, group in segments if s_ == 0]
     timecov = np.sum(s_segments) / len(np.where(labels != 0)[0])
-    d["state"] = "unlabeled"
-    d["timecov"] = timecov
-    d_["unlabeled_timecoverage"] = timecov
+    d['state'] = 'unlabeled'
+    d['timecov'] =  timecov
+    d_['unlabeled_timecoverage'] = timecov
     ds.append(d)
-
+    
     if state_as_index:
-        return ds
+        return(ds)
     else:
-        return d_
+        return(d_)
+
--- pycrostates/viz/viz.py
+++ pycrostates/viz/viz.py
@@ -1,18 +1,15 @@
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
-import mne
-
 from typing import Tuple
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import mne
+import numpy as np
 
-def plot_segmentation(
-    labels: np.ndarray,
-    raw: mne.io.RawArray,
-    names: list = None,
-    tmin: float = 0.0,
-    tmax: float = None,
-) -> Tuple[mpl.figure.Figure, mpl.axes.Axes]:
+
+def plot_segmentation(labels: np.ndarray, raw: mne.io.RawArray,
+                      names: list = None,
+                      tmin: float = 0.0, tmax: float = None) -> Tuple[mpl.figure.Figure,
+                                                                      mpl.axes.Axes]:
     """[summary]
 
     Args:
@@ -32,27 +29,28 @@ def plot_segmentation(
     times = raw_.times + tmin
     n_states = np.max(labels) + 1
     if names is None:
-        names = ["unlabeled"]
-        names.extend([f"Microstate {i+1}" for i in range(n_states - 1)])
-    labels = labels[(times * raw_.info["sfreq"]).astype(int)]
-    cmap = plt.cm.get_cmap("plasma", n_states)
+        names = ['unlabeled']
+        names.extend([f'Microstate {i+1}' for i in range(n_states - 1)])
+    labels = labels[(times * raw_.info['sfreq']).astype(int)]
+    cmap = plt.cm.get_cmap('plasma', n_states)
 
-    fig = plt.figure(figsize=(10, 4))
-    ax = plt.plot(times, gfp, color="black", linewidth=0.2)
+    fig = plt.figure(figsize=(10,4))
+    ax = plt.plot(times, gfp, color='black', linewidth=0.2)
     for state, color in zip(range(n_states), cmap.colors):
         w = np.where(labels[1:] == state)
-        a = np.sort(np.append(w, np.add(w, 1)))
+        a = np.sort(np.append(w,  np.add(w, 1)))
         x = np.zeros(labels.shape)
         x[a] = 1
         x = x.astype(bool)
-        plt.fill_between(times, gfp, color=color, where=x, step=None, interpolate=False)
+        plt.fill_between(times, gfp, color=color,
+                        where=x, step=None, interpolate=False)
     norm = mpl.colors.Normalize(vmin=0, vmax=n_states)
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
     cbar = plt.colorbar(sm, ticks=[i + 0.5 for i in range(n_states)])
     cbar.ax.set(yticklabels=names)
-    plt.xlabel("Time (s)")
-    plt.title("Segmentation")
+    plt.xlabel('Time (s)')
+    plt.title('Segmentation')
     plt.autoscale(tight=True)
     plt.show()
-    return (fig, ax)
+    return(fig, ax)
