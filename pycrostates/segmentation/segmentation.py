1f2be0f6e6a6e83320c21feb5004750080ed2960
vferat <victor.ferat@live.fr>
2021-06-28 18:12:03 +0200

    Update segmentation.py

--- pycrostates/segmentation/segmentation.py
+++ pycrostates/segmentation/segmentation.py
@@ -37,7 +37,6 @@ class RawSegmentation(BaseSegmentation):
     def plot(self, tmin: float = 0.0, tmax: float = None):
         fig, ax = plot_segmentation(segmentation=self.segmentation,
                                     inst=self.inst,
-                                    cluster_centers=self.cluster_centers,
                                     names=self.names,
                                     tmin=tmin,
                                     tmax=tmax)
