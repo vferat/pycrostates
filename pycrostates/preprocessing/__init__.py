2ea46da504a028655229584c33b2add670529e71
vferat <victor.ferat@live.fr>
2021-06-30 19:44:17 +0200

    Update __init__.py

--- pycrostates/preprocessing/__init__.py
+++ pycrostates/preprocessing/__init__.py
@@ -1,3 +1,3 @@
 from .preprocessing import _extract_gfps, resample, extract_gfp_peaks
 
-__all__ = (_extract_gfps, resample, extract_gfp_peaks)
+__all__ = ('_extract_gfps', 'resample', 'extract_gfp_peaks')
