2de9a29e7b7d15d97323147b8f6c10689d78c1dc
vferat <victor.ferat@live.fr>
2021-06-30 19:31:06 +0200

    Update __init__.py

--- pycrostates/__init__.py
+++ pycrostates/__init__.py
@@ -3,3 +3,9 @@ from . import metrics
 from . import viz
 from . import utils
 from . import preprocessing
+
+__all__ = ('clustering',
+           'metrics',
+           'viz',
+           'utils',
+           'preprocessing')
