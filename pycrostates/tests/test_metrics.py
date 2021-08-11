45eac6d7fe758d833dc6a7e6feb20e2b9f4b317c
Victor Férat <victor.ferat@live.Fr>
2021-08-11 11:38:02 +0200

    Update test_metrics.py

--- pycrostates/tests/test_metrics.py
+++ pycrostates/tests/test_metrics.py
@@ -5,17 +5,30 @@ import mne
 from mne.datasets import testing
 
 from pycrostates.clustering import ModKMeans
-from pycrostates.metrics import silhouette
+from pycrostates.metrics import silhouette, davies_bouldin, calinski_harabasz, dunn
 
 data_path = testing.data_path()
 fname_raw_testing = op.join(data_path, 'MEG', 'sample',
                             'sample_audvis_trunc_raw.fif')
 
+raw = mne.io.read_raw_fif(fname_raw_testing, preload=True)
+raw = raw.pick('eeg')
+n_clusters = 4
+ModK = ModKMeans(n_clusters=n_clusters, random_state=1)
+ModK.fit(raw, n_jobs=1)
+
 def test_silouhette():
-    raw = mne.io.read_raw_fif(fname_raw_testing, preload=True)
-    raw = raw.pick('eeg')
-    n_clusters = 4
-    ModK = ModKMeans(n_clusters=n_clusters, random_state=1)
-    ModK.fit(raw, n_jobs=1)
-    s = silhouette(ModK)
-    print(s)
+    score = silhouette(ModK)
+    assert isinstance(score, float)
+    
+def test_davies_bouldin():
+    score = davies_bouldin(ModK)
+    assert isinstance(score, float)
+    
+def test_calinski_harabasz():
+    score = calinski_harabasz(ModK)
+    assert isinstance(score, float)
+    
+def test_dunn():
+    score = dunn(ModK)
+    assert isinstance(score, float)
