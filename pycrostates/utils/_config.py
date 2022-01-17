15937855a8f6e3ad8c2fe773865d9a8bac62f94e
Victor Férat <victor.ferat@live.Fr>
2022-01-17 11:25:48 +0100

    Fix style

--- pycrostates/utils/_config.py
+++ pycrostates/utils/_config.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+
 def _get_home_dir():
     """Get home directory"""
     if os.name.lower() == 'nt':
@@ -13,17 +14,23 @@ def _get_home_dir():
         os.mkdir(home_dir)
     return(home_dir)
 
+
 def _get_config_path():
     """Get config path"""
     home_dir = _get_home_dir()
     config_path = os.path.join(home_dir, 'pycrostates.json')
     return(config_path)
 
-default_config = {'LEMON_DATASET_PATH' :  os.path.join(_get_home_dir(), 'pycrostates_data', 'LEMON')}
+
+default_config = {'LEMON_DATASET_PATH': os.path.join(_get_home_dir(),
+                                                     'pycrostates_data',
+                                                     'LEMON')}
+
 
 def _save_config(config):
     with open(_get_config_path(), 'w') as f:
         json.dump(config, f)
+
 
 def get_config():
     config_path = _get_config_path()
@@ -31,13 +38,12 @@ def get_config():
         # create default config
         _save_config(default_config)
     with open(config_path, 'r') as f:
-        config =  json.load(f)
+        config = json.load(f)
     return(config)
+
 
 def set_config(key, value):
     config = get_config()
     if key in default_config.keys:
         config[key] = value
     _save_config(config)
-
-    
