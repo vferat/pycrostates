a8efead6a07f5d3a578d4f433be85e66872b19a9
vferat <victor.ferat@live.fr>
2022-01-18 14:42:37 +0100

    Update plot_modK.py
    Update plot_modK.py

--- tutorials/clustering/plot_modK.py
+++ tutorials/clustering/plot_modK.py
@@ -45,7 +45,7 @@ ModK.get_cluster_centers_as_raw()
 
 # %%
 # Clusters centers can be reordered using :meth:`ModK.reorder`:
-ModK.reorder([3, 2, 0, 4, 1])
+ModK.reorder([3, 2, 4, 0, 1])
 ModK.plot()
 
 # %%
