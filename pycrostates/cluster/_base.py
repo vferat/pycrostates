6eb729996744bd9c681c305cd947cd78e2b5ad82
vferat <victor.ferat@live.fr>
2022-03-16 21:22:19 +0100

    updates reorder _labels_

--- pycrostates/cluster/_base.py
+++ pycrostates/cluster/_base.py
@@ -271,8 +271,8 @@ class _BaseCluster(ABC):
         # re-order
         self._cluster_centers_ = self._cluster_centers_[order]
         self._clusters_names = [self._clusters_names[k] for k in order]
-        new_labels = np.zeros(self._labels_.shape)
-        if hasattr(self, '_labels_'):
+        if self._labels_ is not None:
+            new_labels = np.zeros(self._labels_.shape)
             for k in range(0, self.n_clusters):
                 new_labels[self._labels_ == k+1] = order[k] + 1
             self._labels_ = new_labels
