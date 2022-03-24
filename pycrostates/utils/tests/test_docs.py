512ff41220c8602bfa5f9d9f997436077a92690d
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-03-24 15:51:48 +0100

    add basic tests for _docs.py

---
+++ pycrostates/utils/tests/test_docs.py
@@ -0,0 +1,37 @@
+"""Test _docs.py"""
+
+from pycrostates.utils._docs import fill_doc, copy_doc
+
+
+def test_fill_doc():
+    """Test decorator to fill docstring."""
+
+    @fill_doc
+    def foo(n_clusters, verbose):
+        """My doc.
+
+        Parameters
+        ----------
+        %(n_clusters)s
+        %(verbose)s
+        """
+        pass
+
+    assert 'n_clusters : int' in foo.__doc__
+    assert 'verbose : bool' in foo.__doc__
+
+
+def test_copy_doc():
+    """Test decorator to copy docstring."""
+
+    def foo(x, y):
+        """
+        My doc.
+        """
+        pass
+
+    @copy_doc(foo)
+    def foo2(x, y):
+        pass
+
+    assert 'My doc.' in foo2.__doc__
