6dcc7fefe86b4845c10341e5f459a3334d5aed08
Victor Férat <victor.ferat@live.Fr>
2022-05-16 09:45:29 +0100

    Fix tests

--- pycrostates/preprocessing/tests/test_resample.py
+++ pycrostates/preprocessing/tests/test_resample.py
@@ -26,7 +26,7 @@ epochs = mne.make_fixed_length_epochs(raw, duration=2, preload=True)
     ],
 )
 def test_resample(inst, replace):
-    resamples = resample(inst, 10, 500, replace=replace)
+    resamples = resample(inst, n_epochs=10, n_samples=500, replace=replace)
     assert isinstance(resamples, list)
     assert len(resamples) == 10
     for r in resamples:
@@ -66,6 +66,6 @@ def test_resample_raw_samples_coverage(inst):
 
 def test_resample_raw_noreplace_error():
     try:
-        resample(raw, 1000, 5000, replace=False)
+        resample(raw, n_epochs=1000, n_samples=5000, replace=False)
     except Exception as e:
         assert isinstance(e, ValueError)
