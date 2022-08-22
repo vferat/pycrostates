e0708fb5d6b4889a5a57667d491e80637caa03c2
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-08-22 14:55:28 +0200

    better html title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,7 +93,7 @@ default_role = "py:obj"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "pydata_sphinx_theme"
-
+html_title = f"{project} {release}"
 html_theme_options = {
     "icon_links": [
         {
