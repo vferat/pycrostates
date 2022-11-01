a912e9f76c3e680a6c23c9716d070108381410ef
Victor Ferat <victor.ferat@live.Fr>
2022-11-01 13:09:52 +0100

    Update tutorial

--- tutorials/clustering/plot_modK.py
+++ tutorials/clustering/plot_modK.py
@@ -8,6 +8,7 @@ K-means algorithm is based on :footcite:t:`Marqui1995`.
 """
 
 #%%
+# .. _fit:
 # .. include:: ../../../../links.inc
 
 # %%
@@ -99,58 +100,3 @@ ModK.plot()
 
 ModK.invert_polarity([False, False, True, True, False])
 ModK.plot()
-
-#%%
-# Finally, the modified K-means\ :footcite:p:`Marqui1995` can be used to
-# predict the microstates segmentation using the
-# :meth:`pycrostates.cluster.ModKMeans.predict` method.
-# By default, segments annotated as bad will not be labeled, but this behavior
-# can be changed with the ``reject_by_annotation`` argument. Smoothing can be
-# performed on the output sequence by setting the ``factor`` argument ``> 0``
-# (no smoothing by default ``factor=0``) while the ``half_window_size``
-# parameter is used to specify the smoothing temporal span. Finally, the
-# ``reject_edges`` argument is used to prevent the assignment of the first and
-# last segment of each recording (or each epoch) as these can be incomplete. It
-# should have little impact for `~mne.io.Raw` objects, but can be important
-# when working with `~mne.Epochs`.
-
-segmentation = ModK.predict(raw, reject_by_annotation=True, factor=10,
-                            half_window_size=10, min_segment_length=5,
-                            reject_edges=True)
-segmentation.plot(tmin=1, tmax=5)
-#%%
-# `~pycrostates.segmentation.RawSegmentation` and
-# `~pycrostates.segmentation.EpochsSegmentation`
-# are placeholders for microstate segmentations.
-# Labels of each datapoints are stored in the ``_labels`` attribute
-
-segmentation._labels
-
-#%%
-# Microstate parameters can be extract form segmentations using the
-# `~pycrostates.segmentation.RawSegmentation.compute_parameters` methods
-
-parameters = segmentation.compute_parameters()
-parameters
-#%%
-# and probabilities of microstate transitions can be obtained using the 
-# `~pycrostates.segmentation.RawSegmentation.compute_transition_probabilities`
-# method
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-T = segmentation.compute_transition_probabilities()
-
-ax = sns.heatmap(T,
-                 annot=True,
-                 cmap='Blues',
-                 xticklabels=segmentation.cluster_names,
-                 yticklabels=segmentation.cluster_names)
-ax.set_ylabel('From')
-ax.set_xlabel('To')
-
-plt.show()
-#%%
-# References
-# ----------
-# .. footbibliography::
