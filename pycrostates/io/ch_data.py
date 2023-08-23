c3d3de43c816279f4dd0322b67270f7af7966597
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-08-23 11:50:39 +0200

    fix missed for _picks_to_idx

--- pycrostates/io/ch_data.py
+++ pycrostates/io/ch_data.py
@@ -4,8 +4,13 @@ from typing import Any, Union
 
 import numpy as np
 from mne import Info, pick_info
-from mne.io.pick import _picks_to_idx
+from mne.utils import check_version
 from numpy.typing import NDArray
+
+if check_version("mne", "1.6"):
+    from mne._fiff.pick import _picks_to_idx
+else:
+    from mne.io.pick import _picks_to_idx
 
 from .._typing import CHData, CHInfo
 from ..utils._checks import _check_type
