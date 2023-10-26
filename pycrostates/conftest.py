6557d58c4e36f8f12c2643129059549f1356aa23
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-10-26 17:04:41 +0200

    skip conftest in coverage and ignore pytest warn

--- pycrostates/conftest.py
+++ pycrostates/conftest.py
@@ -7,16 +7,19 @@ from pycrostates import set_log_level
 
 def pytest_configure(config):
     """Configure pytest options."""
+    if "MPLBACKEND" not in os.environ:
+        os.environ["MPLBACKEND"] = "agg"
+
     warnings_lines = r"""
     error::
     # We use matplotlib agg backend to avoid any window to pop up during tests.
     ignore:Matplotlib is currently using agg:UserWarning
+    ignore:datetime\.utcfromtimestamp.*is deprecated:DeprecationWarning
     """
-    if "MPLBACKEND" not in os.environ:
-        os.environ["MPLBACKEND"] = "agg"
     for warning_line in warnings_lines.split("\n"):
         warning_line = warning_line.strip()
         if warning_line and not warning_line.startswith("#"):
             config.addinivalue_line("filterwarnings", warning_line)
+
     set_log_level_mne("WARNING")
     set_log_level("WARNING")
