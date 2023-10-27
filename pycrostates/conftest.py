cb155e0f07c0d52854dd03b85347445460d600b6
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-10-27 17:14:31 +0200

    fix import

--- pycrostates/conftest.py
+++ pycrostates/conftest.py
@@ -4,7 +4,8 @@ import warnings
 import pytest
 from mne import set_log_level as set_log_level_mne
 
-from pycrostates import logger, set_log_level
+from pycrostates import set_log_level
+from pycrostates.utils._logs import logger
 
 
 def pytest_configure(config):
