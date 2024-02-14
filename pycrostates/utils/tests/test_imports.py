855007b11b8270e7ad53ad6676e247ef3d1d182b
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-02-14 14:39:19 +0100

    fix imports tests

--- pycrostates/utils/tests/test_imports.py
+++ pycrostates/utils/tests/test_imports.py
@@ -2,30 +2,23 @@
 
 import pytest
 
-from pycrostates.utils._imports import import_optional_dependency
-from pycrostates.utils._logs import logger, set_log_level
-
-set_log_level("INFO")
-logger.propagate = True
+from .._imports import import_optional_dependency
 
 
-def test_import_optional_dependency(caplog):
+def test_import_optional_dependency():
     """Test the import of optional dependencies."""
     # Test import of present package
     numpy = import_optional_dependency("numpy")
     assert isinstance(numpy.__version__, str)
 
     # Test import of absent package
-    caplog.clear()
     with pytest.raises(ImportError, match="Missing optional dependency"):
         import_optional_dependency("non_existing_pkg", raise_error=True)
-    assert "Missing optional dependency" not in caplog.text
 
     # Test import of absent package without raise
-    caplog.clear()
-    pkg = import_optional_dependency("non_existing_pkg", raise_error=False)
+    with pytest.warns(RuntimeWarning, match="Missing optional dependency"):
+        pkg = import_optional_dependency("non_existing_pkg", raise_error=False)
     assert pkg is None
-    assert "Missing optional dependency" in caplog.text
 
     # Test extra
     with pytest.raises(ImportError, match="blabla"):
